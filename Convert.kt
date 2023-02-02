cc7b07d035a28c2d9dfe8cf15e006bed5e31529b
Foram Patel <foramjpatel5778@gmail.com>
2023-02-02 18:01:56 -0500

    Getting value that would be converted into another unit and source unit from user.

--- Convert.kt
+++ Convert.kt
@@ -2,4 +2,20 @@
 // Student ID:-  A00272251
 // Mobile Application Development (January 2023)
 
+fun main() {
 
+	// try{...}catch{...} for exception handling
+	try{
+	
+		// Get value from user
+		print("\nEnter a number that you want to convert it in a different unit: ")
+		val enteredNumber: Double = readLine()!!.toDouble()
+		
+		// Source unit selection from user
+		println("\n1 -> Second\n2 -> Minute\n3 -> Hour\n4 -> Day\n5 -> Week\n6 -> Month")
+		println("Example:- For 'Second' enter '1' ")
+		print("Please select any source measurent unit: ")
+	
+		var sourceUnit = readLine()!!.toInt()
+	}
+}
